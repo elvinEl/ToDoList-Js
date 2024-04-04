19c0a1b7b6e7b4f4efe80b40a2fbe6eb1a66cef8
Yuzbekov Elvin <yuzbekov79@gmail.com>
2024-04-04 21:16:03 +0400

    complated adding and delating

--- main.js
+++ main.js
@@ -1,22 +1,52 @@
 //////////////////DELETED TODOS /////////////////////////////////
-const deletedTodos = document.querySelectorAll(".delete-item");
-deletedTodos.forEach(function (button) {
-  button.addEventListener("click", function () {
-    const deleted = button.closest("li");
-    deleted.remove();
+function addDeleteListener() {
+  const deletedTodos = document.querySelectorAll(".delete-item");
+  deletedTodos.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const deleted = button.closest("li");
+      deleted.remove();
+    });
   });
-});
+}
+
 /////////////////////DELETED ALL TODOS////////////////////////////////
 const deletedAllTodos = document.querySelector("#clearButton");
 deletedAllTodos.addEventListener("click", function (event) {
   event.preventDefault();
   const listGroup = document.querySelector(".list-group");
-  listGroup.remove();
+  const test = Array.from(listGroup.children);
+  test.forEach(function (item) {
+    item.remove();
+  });
 });
 ////////////////////ADDED TODOS////////////////////////////////
 const inputElement = document.querySelector("#todoName");
-inputElement.addEventListener("input",function(){
-    const value = inputElement.value
-    console.log(value);
-})
 console.log(inputElement);
+// inputElement.addEventListener("input", function (e) {
+//   console.log(e.target);
+// });
+
+const addButton = document.querySelector("#todoAddButton");
+addButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const value = inputElement.value;
+  if (value.trim() === "") {
+    return;
+  }
+  const liElement = document.createElement("li");
+  liElement.className = "list-group-item d-flex justify-content-between";
+  liElement.textContent = value;
+  const aElement = document.createElement("a");
+  aElement.href = "#";
+  aElement.className = "delete-item";
+  const iElement = document.createElement("i");
+  iElement.className = "fa fa-remove";
+
+  aElement.appendChild(iElement);
+  liElement.appendChild(aElement);
+
+  const listGroup = document.querySelector(".list-group");
+  listGroup.appendChild(liElement);
+  inputElement.value = "";
+  addDeleteListener();
+});
